e01ecb65761e14d04dd7ac6339483b996cc962ef
Liubomyr Klymiuk <53624470+LyubomyrKl@users.noreply.github.com>
2026-09-22 19:06:51 +0200

    Add: design-word-search-data-structure - submission-5

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-5.js
@@ -0,0 +1,52 @@
+class TrieNode {
+    constructor() {
+        this.children = new Map();
+        this.isEnd = false;
+    }
+}
+
+class WordDictionary {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    addWord(word) {
+        let current = this.root;
+
+        for(const char of word){
+            if(!current.children.has(char)){
+                current.children.set(char, new TrieNode())
+            }
+
+            current = current.children.get(char);
+        }
+
+        current.isEnd = true;
+    }
+
+    search(word) {
+        const dfs = (node, i) => {
+            if (i === word.length) return node.isEnd;
+
+            const char = word[i];
+
+            if(char !== '.'){
+                if(node.children.has(char)){
+                    return dfs(node.children.get(char), i + 1)
+                } 
+
+                return false;
+            }
+
+            for(let child of node.children.values()){
+                if(dfs(child, i + 1)){
+                    return true;
+                } 
+            }
+
+            return false;
+        }
+
+       return dfs(this.root, 0)
+    }
+}
